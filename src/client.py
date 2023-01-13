ccef6afadb4eb59d6aedb2d95debc9abd109b292
dapedu <pedrosantosdevelop@gmail.com>
2023-01-13 17:42:41 -0300

    atualiza client e user com comandos utilitarios

--- src/client.py
+++ src/client.py
@@ -1,5 +1,6 @@
 # Cliente TCP
 import socket
+from threading import Thread
 
 
 class Client:
@@ -17,6 +18,11 @@ class Client:
         self.tcp_socket.connect((self.host, self.port))
         client.send_message(self.client_name.encode())
 
+        t_receive_messages = Thread(target=self.receive_messages_loop)
+        t_send_messages = Thread(target=self.send_messages_loop)
+        t_receive_messages.start()
+        t_send_messages.start()
+
     def send_message(self, msg):
         """
         Sends message to the server
@@ -24,17 +30,27 @@ class Client:
         """
         self.tcp_socket.send(msg)
 
+    def receive_message(self):
+        return self.tcp_socket.recv(1024).decode()
+
     def close_connection(self):
         """Closes connection with the server"""
         self.tcp_socket.close()
 
+    def receive_messages_loop(self):
+        while True:
+            msg = self.receive_message()
+            print(msg)
+
+    def send_messages_loop(self):
+        while True:
+            msg = input()
+            if msg == "\x18" or msg == "End":
+                self.close_connection()
+                break
+            self.send_message(msg.encode())
+        pass
+
 
 client = Client("127.0.0.1", 5002, "Desktop top")
 client.connect()
-
-while True:
-    msg = input()
-    if msg == "End":
-        client.close_connection()
-        break
-    client.send_message(msg.encode())
--- src/user.py
+++ src/user.py
@@ -1,25 +1,27 @@
 class User:
     count = 0
 
-    def __init__(self, host, port, client_name, connection) -> None:
+    def __init__(self, host, port, client_name, connection):
         User.count += 1
         self.host = host
         self.port = port
         self.client_name = client_name
         self.connection = connection
         self.nick = f"Usuário {User.count}"
+        self.current_channel = None
 
-    def setNick(self, nick):
+    def set_nick(self, nick: str, nicks: list[str]):
+        if nick in nicks:
+            return False
+
         self.nick = nick
-        # Adicionar verificação se o nick é único
+        return True
 
-    def set_current_channel(self, channel_name):
+    def set_current_channel(self, channel_name: str):
         self.current_channel = channel_name
 
-    def getSelf(self):
-        return {
-            "host": self.host,
-            "port": self.port,
-            "client_name": self.client_name,
-            "nick": self.nick
-        }
+    def quit_current_channel(self):
+        self.current_channel = None
+
+    def get_user(self):
+        return f"{self.nick} {self.host} {self.client_name}"
