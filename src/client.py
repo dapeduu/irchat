0e82377cd613b4ad09a2fe95c549528d6752ef2c
dapedu <pedrosantosdevelop@gmail.com>
2023-01-15 11:54:10 -0300

    encerra o client quando o usuário sai

--- src/client.py
+++ src/client.py
@@ -12,11 +12,13 @@ class Client:
         self.tcp_socket = socket.socket(socket.AF_INET,
                                         socket.SOCK_STREAM)
         self.client_name = client_name
+        self.running = False
 
     def connect(self):
         """Connects with the server on the host and ports initialized and send the client name"""
         self.tcp_socket.connect((self.host, self.port))
         client.send_message(self.client_name.encode())
+        self.running = True
 
         t_receive_messages = Thread(target=self.receive_messages_loop)
         t_send_messages = Thread(target=self.send_messages_loop)
@@ -35,21 +37,22 @@ class Client:
 
     def close_connection(self):
         """Closes connection with the server"""
+        self.running = False
         self.tcp_socket.close()
 
     def receive_messages_loop(self):
-        while True:
+        while self.running:
             msg = self.receive_message()
             print(msg)
 
     def send_messages_loop(self):
-        while True:
+
+        while self.running:
             msg = input()
-            if msg == "\x18" or msg == "End":
-                self.close_connection()
-                break
             self.send_message(msg.encode())
-        pass
+
+            if msg == "\x18" or msg == "QUIT":
+                self.running = False
 
 
 client = Client("127.0.0.1", 5002, "Desktop top")
